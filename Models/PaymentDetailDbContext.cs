5a328b0e3475b4570334617ca31f7a4dcacaee55
mabashambs <mbsmabasha@gmail.com>
2024-06-16 17:41:17 +0530

    Add project files.

---
+++ Models/PaymentDetailDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentWebApp.Models
+{
+    public class PaymentDetailDbContext : DbContext
+    {
+        public PaymentDetailDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
